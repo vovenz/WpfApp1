776341636abd715dfa33b536e8a780bbcd8c6d34
William Rezidium <vovaburachenoc@gmail.com>
2023-04-08 09:46:10 +0500

    allDone?();

--- WpfApp1/Page2.xaml.cs
+++ WpfApp1/Page2.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -46,6 +49,39 @@ namespace WpfApp1
                 change_class.ThemeChanged = false;
             }
             change_class.isAction1 = !change_class.isAction1;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //login();
+            string login = textBox1.Text;
+            string password = textBox2.Text;
+            string root = textBox3.Text;
+            string myConnectionString = @"server=WILLIAM;Trusted_Connection=Yes;DataBase=Database;";
+            string mySelectQuery = "SELECT [login] FROM [dbo].[users] WHERE [login] = '" + login + "'and [password]='" + password + "'and [root]='" + root + "'";
+            SqlConnection myConnection = new SqlConnection(myConnectionString);
+            SqlCommand cmd = new SqlCommand(mySelectQuery, myConnection);
+            try
+            {
+                myConnection.Open();
+                object obj = cmd.ExecuteScalar();
+                if (obj == null || obj == DBNull.Value)
+                {
+                    MessageBox.Show("Такого пользователя нет");
+                }
+                else
+                {
+                    MessageBox.Show("Есть такой профиль");
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("При входе в систему пользователя '" + login + "' произошла ошибка: " + ex.Message);
+            }
+            finally
+            {
+                myConnection.Close();
+            }
         }
     }
 }
